ebb03bc6e3adc0c8ee016311e8d64047c9f34a1c
Hamid khan <hamidabc311@gmail.com>
2026-09-02 15:44:38 +0500

    All variable and its type

--- variable.py
+++ variable.py
@@ -1,1 +1,77 @@
 # this is variable
+"""
+variable definition
+variable is a name used to store data in a memory
+"""
+# variable declaration
+name = "hamid"
+print (name )
+age = "18"
+print (age)
+
+
+# types of varible 
+# first type is srting 
+# string is a collection of character
+print   ("=============================")
+name =  "hamid khan"
+instiute_name=" I am the student of city universty of information and technolgy peshawer"
+# now i write all data in string type 
+print   ( "My name is :",name)
+print   ( "My instute name : ",instiute_name)
+print   ("=============================") 
+
+
+
+# the second type is integer
+#integer is a whole number without decimal point
+age = int (18)
+print ("my age is :",age)
+print ("============================")
+
+ # the third type is float
+# float is a number with decimal point
+height = float (5.6)
+print ("my height is :",height)
+print ("============================")
+
+# the fourth type is boolean
+# boolean is a type that can have only two values: True or False
+is_student= True
+print ("am i student :",is_student)
+print ("============================")
+
+# the fifth is none type
+# none is a type that represents the absence of a value
+hamid = None
+print ("hamid is :",hamid)
+print ("============================")
+
+# now the variable is compleet
+
+
+
+# multiple assignment
+# what is multiple assignment
+# in this we store alot of varible in single line 
+x=3
+v=4
+u=8
+print ("the value of x :" ,x)
+print ("the value of v:" ,v)
+print ("the value of u:",u)
+
+# varible in expression
+result = x + v + u
+print ("the result is :",result)
+
+# print vatiable type
+a =10
+g= 5.6
+c= "hamid"
+print (" the type of a is :" ,type(a))
+
+print ( "the type of g is :", type(g))
+
+print ("the type of c is :" ,type (c))
+
