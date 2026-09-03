30f39c68c5ec2e30d25f82a4fc3ed819b576e3b4
Hamid khan <hamidabc311@gmail.com>
2026-09-03 16:16:00 +0500

    ALL DATA TYPES WITH EXAMPLE

---
+++ Data_types.py
@@ -0,0 +1,36 @@
+# Data types of python
+int
+float
+str
+bool
+None
+
+
+# firsr is int data type in this we stote numer without decimal
+x = 5 # so we store 5 bcz 5 is not decimal number
+print ("the value of x is :",x)
+# the anser will be 5
+
+# second is float data type in this we store number with decimal
+x = 5.6 # so we store 5.6 bcz 5.6 is decimal number
+print ("the value of x is :",x)
+# the answer will be 5.6
+
+# third is str data type in this we store text or string
+x = "Hello, World!"
+print ("the value of x is :",x)
+# the answer will be "Hello, World!"
+
+
+# fourth is bool data type in this we store True or False
+x = True # so we store True
+print ("the value of x is :",x)
+# the answer will be True
+
+# fifth is None data type in this we store None
+x = None # so we store None
+print ("the value of x is :",x)
+# the answer will be None
+
+# these are the five data types of python
+ss
