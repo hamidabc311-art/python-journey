c772b501e86ee16807c1b94e4aa392d135bc6f6c
Hamid khan <hamidabc311@gmail.com>
2026-09-04 23:31:21 +0500

    operator and all type with example

---
+++ operator.py
@@ -0,0 +1,47 @@
+# today i learn all type of operators in python
+
+
+print("===========================================")
+print ("1. Arithmetic operators")
+# in this we perform basic mathimatical operatiion
+
+print (" ADDTION")
+x=2
+y=4
+print ("THE ANSWER OF X AND Y IS :" ,(x+y)) # anwser = 6
+print("===========================================")
+
+print (" SUBTRICTION")
+a = 7
+b= 9
+print ("THE ANSWER OF a AND b IS :" ,(a-b)) # anwser = -2
+print("===========================================")
+
+print (" MULTIPLACTION")
+c = 5
+d = 6
+print ("THE ANSWER OF c AND d IS :" ,(c*d)) # anwser = 30
+print("===========================================")
+
+print (" DIVISION")
+e = 10
+f = 2
+print ("THE ANSWER OF e AND f IS :" ,(e/f)) # anwser = 5.0
+print("===========================================")
+
+print (" MODULUS") # reminder 
+g = 10
+h = 3
+print ("THE ANSWER OF g AND h IS :" ,(g%h)) # anwser = 1
+print("===========================================")
+
+print (" EXPONENTIAL")
+i = 2
+j = 3
+print ("THE ANSWER OF i AND j IS :" ,(i**j)) # anwser = 8
+print("===========================================")
+
+print (" FLOOR DIVISION") # means how many times the divisor fits into the dividend
+k = 17
+l = 2
+print ("THE ANSWER OF k AND l IS :" ,(k//l)) # anwser = 2
